316bc9f99822168605f4dd98a2c1278e48e65a97
Ordo Adrian <ordoadrian@Ordos-MacBook-Pro.local>
2023-01-25 10:20:46 +0000

    added functionality that grabs the temperature and converts it from K to C

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
 const apiKey = '24911910bacb8ce7dbbf8928a170d9c6';
 let city = 'manchester';
 let country = 'gb';
+let dateToday = moment().format('DD/MM/YYYY');
+let date = dateToday + 1;
+let weatherIcon = '';
+let temperatue = '';
+let humidity = 0;
+let windSpeed = 0;
 
-// TODO: This returns the temperatue in Kelvin, need to convert it into Celsius
+console.log(`This is todays date: ${dateToday}`);
+
+
+// TODO: This returns the temperatue in Kelvin, need to convert it into Celsius - 0°C = 273.15K
 let weatherDataURL = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${apiKey}`
 
 
@@ -13,7 +22,9 @@ function getWeatherData() {
         url: weatherDataURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response)
+        temperatue = Math.floor(response.main.temp - 273.15)
+        console.log(`${temperatue}°C`)
+        // response.main.temp
       });
 };
 getWeatherData();
